aa5ebb1afc2920a76de8c41421710b0496b0de05
TonySN90 <tony.heider90@gmail.com>
2023-12-02 23:53:35 +0100

    add hide/close functions

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 "use strict";
 
 import "./../scss/main.scss";
+import * as view from "./view.js";
+
+const openInputFieldBtn = document.querySelector(".button__open");
+const storeBtn = document.querySelector(".button__store");
+
+const state = {};
+
+openInputFieldBtn.addEventListener("click", view.displayInputField);
+storeBtn.addEventListener("click", view.closeInputField);
--- src/js/view.js
+++ src/js/view.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const inputField = document.querySelector("#inputField");
+
+export const displayInputField = function () {
+  inputField.style.width = "80%";
+};
+
+export const closeInputField = function () {
+  inputField.style.width = "0%";
+};
